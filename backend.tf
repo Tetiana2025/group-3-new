1ef6b6b034c166ef4e7e29c503b2e4c469db1220
Tetiana2025 <tetianabahlai@gmail.com>
2025-04-30 16:43:58 +0000

    added group-3-new files

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-bucket-group-3"
+    key            = "env/dev/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
