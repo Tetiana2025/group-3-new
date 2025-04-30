a1e45020fe78ddf5e8594ee76d8b175ee557ed66
Tetiana2025 <tetianabahlai@gmail.com>
2025-04-30 18:37:11 +0000

    first module version

--- variables.tf
+++ variables.tf
@@ -8,3 +8,5 @@ variable "bastion_cidr"     { default = "0.0.0.0/0" }
 variable "db_name"          { default = "wordpress" }
 variable "db_user"          { default = "Kaizen" }
 variable "db_password"      {}
+variable "db_instance_class"{ type    = string }
+variable "engine_version"   { type    = string }
