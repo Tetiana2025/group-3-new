f5c0e8f31468351c5ce3ad4137aa196346718bb7
Tetiana2025 <tetianabahlai@gmail.com>
2025-05-01 04:31:11 +0000

    second module version with changes

--- modules/scripts/wordpress.sh
+++ modules/scripts/wordpress.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-apt update -y
-apt install apache2 php php-mysql wget unzip -y
-cd /var/www/html
-wget https://wordpress.org/latest.tar.gz
-tar -xvzf latest.tar.gz
-cp -r wordpress/* .
-rm -rf wordpress latest.tar.gz
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+
+	sudo yum install httpd -y
+	sudo systemctl status http
+	sudo systemctl start httpd
+	sudo systemctl enable httpd
+	sudo dnf install wget php-mysqlnd httpd php-fpm php-mysqli mariadb105-server php-json php php-devel -y
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzf latest.tar.gz
+	sudo mv wordpress/* /var/www/html
+	sudo chown -R apache:apache /var/www/html
+	sudo chmod -R 755 /var/www/html
+	sudo rm -r /var/www/html/index.html
+	sudo systemctl restart httpd
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region"       { default = "us-east-1" }
 variable "vpc_cidr"         { default = "10.0.0.0/16" }
 variable "azs"              { default = ["us-east-1a", "us-east-1b", "us-east-1c"] }
-variable "ami_id"           { default = "ami-0f9de6e2d2f067fca" }
+variable "ami_id"           { default = "ami-0f88e80871fd81e91" }
 variable "instance_type"    { default = "t2.micro" }
 variable "public_key_path"  { default = "~/.ssh/id_rsa.pub" }
 variable "bastion_cidr"     { default = "0.0.0.0/0" }
